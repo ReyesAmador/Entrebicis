9352df73031e0f4976a9ed7fdc5d7e0f1da7392d
Reyes <ramador@alumnat.copernic.cat>
2025-03-20 22:43:07 +0100

    modificado el logout para que cierre sesión y puesto el botón con un formulario post

--- Backend/entrebicis/src/main/java/cat/copernic/entrebicis/configuration/SecurityConfig.java
+++ Backend/entrebicis/src/main/java/cat/copernic/entrebicis/configuration/SecurityConfig.java
@@ -38,6 +38,8 @@ public class SecurityConfig {
             .logout(logout -> logout
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/login?logout")
+                .invalidateHttpSession(true) // Invalida la sesión
+                .deleteCookies("JSESSIONID") // Elimina la cookie de sesión
                 .permitAll()
             );
 
