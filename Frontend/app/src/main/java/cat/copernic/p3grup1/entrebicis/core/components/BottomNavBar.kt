9a233cf13ffce1d1464039e909ae0334f1f3a7da
Reyes <ramador@alumnat.copernic.cat>
2025-03-26 11:01:27 +0100

    cambiado la forma visual del bottomnav

--- Frontend/app/src/main/java/cat/copernic/p3grup1/entrebicis/core/components/BottomNavBar.kt
+++ Frontend/app/src/main/java/cat/copernic/p3grup1/entrebicis/core/components/BottomNavBar.kt
@@ -1,10 +1,22 @@
 package cat.copernic.p3grup1.entrebicis.core.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
@@ -17,14 +29,50 @@ fun BottomNavBar(navController: NavController) {
         BottomNavItem.Profile
     )
 
-    NavigationBar {
-        val currentDestination = navController.currentBackStackEntryAsState().value?.destination
+    val currentDestination = navController.currentBackStackEntryAsState().value?.destination
+
+    NavigationBar(contentColor = Color.White) {
         items.forEach { item ->
+            val isSelected = currentDestination?.route == item.route
+
             NavigationBarItem(
-                icon = { Icon(item.icon, contentDescription = item.label) },
-                label = { Text(item.label) },
-                selected = currentDestination?.route == item.route,
-                onClick = { navController.navigate(item.route) }
+                selected = isSelected,
+                onClick = {
+                    navController.navigate(item.route){
+                        popUpTo(navController.graph.startDestinationId) { saveState = true }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .width(103.dp)
+                            .height(80.dp)
+                            .background(
+                                color = if (isSelected)
+                                    MaterialTheme.colorScheme.tertiary.copy(alpha = 0.25f)
+                                else
+                                    Color.White,
+                            )
+                    ) {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.label,
+                            tint = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.tertiary,
+                            modifier = Modifier.size(28.dp)
+                        )
+                    }
+                },
+                modifier = Modifier.weight(1f),
+                label = null, // Si no quieres texto, o pon `Text(item.label)`
+                alwaysShowLabel = false,
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Color.Transparent, // sin fondo por defecto
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.tertiary
+                )
             )
         }
     }
