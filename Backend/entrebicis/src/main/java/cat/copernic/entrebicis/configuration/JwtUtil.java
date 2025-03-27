374a8ab182aac2b1438aec4ed0ab85b28db6b8c9
Reyes <ramador@alumnat.copernic.cat>
2025-03-27 09:08:20 +0100

    creada clase JwtUtil para gestionar los tokens

---
+++ Backend/entrebicis/src/main/java/cat/copernic/entrebicis/configuration/JwtUtil.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.entrebicis.configuration;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+
+/**
+ *
+ * @author reyes
+ */
+
+@Component
+public class JwtUtil {
+    
+    private final String SECRET_KEY = "entrebicis-secret";
+    
+    public String generateToken(UserDetails userDetails){
+        
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .claim("rol", userDetails.getAuthorities().toString())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24h
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    
+    public String extractUsername(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+    
+    public boolean validarToken(String token, UserDetails userDetails){
+        
+        final String username = extractUsername(token);
+        
+        return username.equals(userDetails.getUsername());
+    }
+    
+}
