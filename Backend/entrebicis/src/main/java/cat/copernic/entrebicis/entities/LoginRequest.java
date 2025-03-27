86be41c64d4f60ded9e373a77ccdd6647775a316
Reyes <ramador@alumnat.copernic.cat>
2025-03-27 09:11:04 +0100

    creadas 2 entidades de DTO

---
+++ Backend/entrebicis/src/main/java/cat/copernic/entrebicis/entities/LoginRequest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.entrebicis.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ *
+ * @author reyes
+ */
+@Data
+public class LoginRequest {
+    
+    private String email;
+    private String paraula;
+    
+}
