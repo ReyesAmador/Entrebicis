8573a65db800ac7f57e1131a8bb27cf80a8a58bc
Reyes <ramador@alumnat.copernic.cat>
2025-04-11 12:47:23 +0200

    creado el viewModel para cargar recompensas propias

---
+++ Frontend/app/src/main/java/cat/copernic/p3grup1/entrebicis/reward/presentation/viewmodel/RewardViewModel.kt
@@ -0,0 +1,39 @@
+package cat.copernic.p3grup1.entrebicis.reward.presentation.viewmodel
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import cat.copernic.p3grup1.entrebicis.core.models.Recompensa
+import cat.copernic.p3grup1.entrebicis.reward.data.repositories.RewardRepo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class RewardViewModel(
+    application: Application,
+    private val rewardRepo: RewardRepo
+): AndroidViewModel(application) {
+
+    private val prefs = application.getSharedPreferences("usuari_prefs", Context.MODE_PRIVATE)
+
+    private val _recompenses = MutableStateFlow<List<Recompensa>>(emptyList())
+    val recompenses: StateFlow<List<Recompensa>> = _recompenses
+
+    private val _error = MutableStateFlow<String?>(null)
+    val error: StateFlow<String?> = _error
+
+    fun carregarRecompenses(){
+        val token = prefs.getString("token", null) ?: return
+        viewModelScope.launch {
+            rewardRepo.getRecompenses(token).fold(
+                onSuccess = {_recompenses.value = it},
+                onFailure = {_error.value = it.message}
+            )
+        }
+    }
+
+    fun clearError() {
+        _error.value = null
+    }
+}
