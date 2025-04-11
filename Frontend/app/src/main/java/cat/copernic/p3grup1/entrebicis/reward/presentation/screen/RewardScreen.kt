d3380417ab4bae567b59f95c4d4e1858f0459c5a
Reyes <ramador@alumnat.copernic.cat>
2025-04-11 14:11:22 +0200

    añadida cabecera y subtitulos

--- Frontend/app/src/main/java/cat/copernic/p3grup1/entrebicis/reward/presentation/screen/RewardScreen.kt
+++ Frontend/app/src/main/java/cat/copernic/p3grup1/entrebicis/reward/presentation/screen/RewardScreen.kt
@@ -5,11 +5,15 @@ package cat.copernic.p3grup1.entrebicis.reward.presentation.screen
 import android.app.Application
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,18 +23,22 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import cat.copernic.p3grup1.entrebicis.core.theme.Primary
 import cat.copernic.p3grup1.entrebicis.reward.presentation.components.RewardCard
 import cat.copernic.p3grup1.entrebicis.reward.presentation.viewmodel.RewardViewModel
 import cat.copernic.p3grup1.entrebicis.reward.presentation.viewmodel.provideRewardViewModelFactory
@@ -52,12 +60,52 @@ fun RewardScreen(
     }
 
     Column(modifier = Modifier.fillMaxSize()) {
-        TopAppBar(title = { Text("Les teves recompenses") }, navigationIcon = {
-            IconButton(onClick = { /* navegar enrere */ }) {
-                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
-            }
-        })
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Primary)
+                .height(184.dp),
+            contentAlignment = Alignment.Center
+        ) {
+                IconButton(onClick = { onBack() },
+                    modifier = Modifier
+                        .align(Alignment.TopStart)
+                        .padding(start = 8.dp, top = 8.dp)) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Botó per anar enrere",
+                        tint = Color.White
+                    )
+                }
+                Text(
+                    text = "Les teves recompenses",
+                    color = Color.White,
+                    style = MaterialTheme.typography.headlineLarge,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = 24.dp)
+                )
+        }
 
+        // Subtítulos
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Recompenses",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Reservar/\nUsuari",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.End
+            )
+        }
         if (error != null) {
             Text(
                 text = error ?: "",
