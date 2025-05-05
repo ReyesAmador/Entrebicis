556d363293c819f12eb0d1b640bb73e4b2c6c733
Reyes <ramador@alumnat.copernic.cat>
2025-05-05 10:33:19 +0200

    cambiado retrofit para que acepte https, manifest también, añadida configuración para aceptar certificados propios

--- Frontend/app/src/main/java/cat/copernic/p3grup1/entrebicis/core/network/RetrofitClient.kt
+++ Frontend/app/src/main/java/cat/copernic/p3grup1/entrebicis/core/network/RetrofitClient.kt
@@ -17,7 +17,7 @@ import java.time.format.DateTimeFormatter
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/api/"
+    private const val BASE_URL = "https://10.0.2.2:8443/api/"
 
     // 🔹 Formato estándar para LocalDate (yyyy-MM-dd)
     @RequiresApi(Build.VERSION_CODES.O)
