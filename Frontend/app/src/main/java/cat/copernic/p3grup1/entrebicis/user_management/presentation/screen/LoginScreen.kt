0cddca311831ca552e5e3ba8c0aab47a6b2b6547
Reyes <ramador@alumnat.copernic.cat>
2025-05-06 09:34:16 +0200

    añadida animación del botón iniciar sessió

--- Frontend/app/src/main/java/cat/copernic/p3grup1/entrebicis/user_management/presentation/screen/LoginScreen.kt
+++ Frontend/app/src/main/java/cat/copernic/p3grup1/entrebicis/user_management/presentation/screen/LoginScreen.kt
@@ -3,6 +3,8 @@ package cat.copernic.p3grup1.entrebicis.user_management.presentation.screen
 import android.app.Application
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -40,6 +42,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -80,6 +83,8 @@ fun LoginScreen(
     val successMessage = savedStateHandle?.get<String>("successMessage")
     val snackbarHostState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
+
+    val scale = remember { Animatable(1f) }
 
     // Si login es correcto, navega
     LaunchedEffect(loginSuccess) {
@@ -211,6 +216,17 @@ fun LoginScreen(
 
             OutlinedButton(
                 onClick = {
+                    scope.launch {
+                        // Animación de pulsación
+                        scale.animateTo(
+                            targetValue = 0.95f,
+                            animationSpec = tween(100)
+                        )
+                        scale.animateTo(
+                            targetValue = 1f,
+                            animationSpec = tween(100)
+                        )
+                    }
                     when {
                         email.isBlank() || password.isBlank() -> {
                             viewModel.clearError()
@@ -234,7 +250,11 @@ fun LoginScreen(
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(48.dp),
+                    .height(48.dp)
+                    .graphicsLayer {
+                        scaleX = scale.value
+                        scaleY = scale.value
+                    },
                 border = BorderStroke(2.dp, Color.White),
                 shape = RoundedCornerShape(8.dp)
             ) {
